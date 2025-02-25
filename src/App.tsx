4c9d24a5f0828118d2f82d0df79adfbf3671c9b6
troubleskiller <troubleskiller@outlook.com>
2025-02-25 20:51:28 +0800

    add some code

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
 /* eslint-disable prettier/prettier */
-// import React, { Fragment } from 'react';
-// import MyGame from './components/myGame';
-// import NewGame from './components/newGame';
-// type Props = {
+import React, { Fragment } from 'react';
+import MyGame from './components/myGame';
+import NewGame from './components/newGame';
+type Props = {
 
-// };
-// export default function App(props: Props) {
-//     return (
-//         <div style={{
-//             height: "auto",
-//             padding: "0 0 50px"
-//         }}>
-//             <div>
-//                 <h1>NewGame</h1>
-//                 <NewGame />
-//             </div>
-//             <div>
-//                 <h1>OldGame</h1>
-//                 <MyGame />
-//             </div>
-//         </div>
-//     )
-// }
-
-import React from 'react';
-
-type Props = {};
-
-const App = (props: Props) => {
-  return <div>App</div>;
 };
+export default function App(props: Props) {
+    return (
+        <div style={{
+            height: "auto",
+            padding: "0 0 50px"
+        }}>
+            <div>
+                <h1>NewGame</h1>
+                <NewGame />
+            </div>
+            <div>
+                <h1>OldGame</h1>
+                <MyGame />
+            </div>
+        </div>
+    )
+}
 
-export default App;
+// import React from 'react';
+//
+// type Props = {};
+//
+// const App = (props: Props) => {
+//   return <div>App</div>;
+// };
+//
+// export default App;
